be06a8c145e6ba8940f68006989c60feb3131312
Tom Sawyer <tomsawyer@TomSawyer.local>
2018-03-21 17:33:08 +0700

    Adding 2 more functions in the Smart Contract, recompile and redeploy it to Rinkeby Testnet for Front End usage

--- ethereum/factory.js
+++ ethereum/factory.js
@@ -3,7 +3,7 @@ import CampaignFactory from './build/CampaignFactory.json';
 
 const instance = new web3.eth.Contract(
   JSON.parse(CampaignFactory.interface),
-  '0xa543d8db89045Cfe905851249f437D8a70126B07'
+  '0xa9EcE450b123e5FE1042cED4776164CD24E253e7'
 );
 
 export default instance;
